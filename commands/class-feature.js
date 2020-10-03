9b6a27a4780c3d869155f28273442c7883ac4feb
Ben Myers <ben.myers@okstate.edu>
2020-10-03 08:33:36 -0500

    Filter class features with the name query param

--- commands/class-feature.js
+++ commands/class-feature.js
@@ -1,7 +1,9 @@
 const fetch = require('node-fetch');
 const usage = require('../replies/usage');
 const {PINK} = require('../utils/colors');
-const {indexify} = require('../utils/sluggify');
+const {indexify, sluggify} = require('../utils/sluggify');
+
+// TODO: Filter out parentheticals in their query and return the first entry in the response
 
 /**
  * Fetch a spell's complete details and convert it into a Discord Embed object
@@ -52,21 +54,19 @@ module.exports = {
 
 		const fullName = args.join(' ');
 		const index = indexify(args);
+		const query = sluggify(args);
 
-		// The /features endpoint doesn't currently support filtering on the ?name param,
-		// so we have do it ourselves.
-		const classFeatures = await fetch('https://www.dnd5eapi.co/api/features/')
-			.then(res => res.json())
-			.then(({results}) => results.filter(feature => feature.index.includes(index)));
+		const classFeatures = await fetch(`https://www.dnd5eapi.co/api/features?name=${query}`)
+			.then(res => res.json());
 
-		const exactMatch = classFeatures.length > 0 && classFeatures.find(feature => feature.index === index);
+		const exactMatch = classFeatures.count > 0 && classFeatures.results.find(feature => feature.index === index);
 
-		if (classFeatures.length === 0) {
+		if (classFeatures.count === 0) {
 			message.reply(`I couldn't find any class features called _${fullName}_. Try again with a shorter query`);
 		} else if (exactMatch) {
 			const featureDetails = await getClassFeatureDetails(exactMatch);
-			if (classFeatures.length > 1) {
-				const alternatives = classFeatures
+			if (classFeatures.count > 1) {
+				const alternatives = classFeatures.results
 					.filter(feature => feature.index !== index)
 					.map(alt => `* ${alt.name}`)
 					.join('\n');
@@ -75,14 +75,14 @@ module.exports = {
 			}
 
 			message.channel.send({embed: featureDetails});
-		} else if (classFeatures.length === 1) {
-			const bestGuess = classFeatures[0];
+		} else if (classFeatures.count === 1) {
+			const bestGuess = classFeatures.results[0];
 			const featureDetails = await getClassFeatureDetails(bestGuess);
 			featureDetails.footer = {text: '---\nThis was my best guess! Feel free to search again!'};
 
 			message.channel.send({embed: featureDetails});
 		} else {
-			const alternatives = classFeatures.map(alt => `* ${alt.name}`);
+			const alternatives = classFeatures.results.map(alt => `* ${alt.name}`);
 			message.reply(`I couldn't find _${fullName}_. Did you mean one of these class features?\n\n${alternatives.join('\n')}`);
 		}
 	}
